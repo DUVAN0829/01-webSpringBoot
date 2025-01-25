18a4f4019e69c4fd7012582c24c7ada4e45bceee
DUVAN0829 <duvancasgonz29@gmail.com>
2025-01-25 12:03:40 -0500

    añade clase 'ParamsDto' como DTO y crea RestController 'RequestParamController'

---
+++ src/main/java/co/duvan/springboot/webapp/springboot_webapp/controllers/RequestParamController.java
@@ -0,0 +1,24 @@
+package co.duvan.springboot.webapp.springboot_webapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.duvan.springboot.webapp.springboot_webapp.model.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamController {
+
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam(required = false, defaultValue = "Hello word!") String message) { // *sirve para enviar valores a tráves de la url */
+
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+
+        return param;
+
+    }
+
+}
---
+++ src/main/java/co/duvan/springboot/webapp/springboot_webapp/model/dto/ParamDto.java
@@ -0,0 +1,16 @@
+package co.duvan.springboot.webapp.springboot_webapp.model.dto;
+
+public class ParamDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
