27150b5ba216a753a59544e6776137d66b1281d7
DUVAN0829 <duvancasgonz29@gmail.com>
2025-01-26 12:17:15 -0500

    Refactoriza controlador 'PathVariable' para inyectar valores de 'application.properties'

--- src/main/java/co/duvan/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
+++ src/main/java/co/duvan/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package co.duvan.springboot.webapp.springboot_webapp.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,20 @@ import co.duvan.springboot.webapp.springboot_webapp.model.dto.ParamDto;
 @RequestMapping("/api/var")
 public class PathVariableController {
 
+    // *Values DI */
+    @Value("${config.username}")
+    private String username;
+
+    // @Value("${config.message}")
+    // private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
+
+    // *Routes */
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
 
@@ -45,5 +60,17 @@ public class PathVariableController {
 
     }
 
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        json.put("code", code);
+
+        return json;
+
+    }
 
 }
