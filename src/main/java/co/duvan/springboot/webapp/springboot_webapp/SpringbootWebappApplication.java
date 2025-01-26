bbd433af92f415a5e529286987a01b7ac32aaf78
DUVAN0829 <duvancasgonz29@gmail.com>
2025-01-26 12:41:31 -0500

    añade 'values.properties' en 'SpringbootWebappApplication'

--- src/main/java/co/duvan/springboot/webapp/springboot_webapp/SpringbootWebappApplication.java
+++ src/main/java/co/duvan/springboot/webapp/springboot_webapp/SpringbootWebappApplication.java
@@ -2,8 +2,11 @@ package co.duvan.springboot.webapp.springboot_webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+//@PropertySource("classpath:values.properties") // todo <- properties normal sin UTF-8 ->
+@PropertySource(value = "classpath:values.properties", encoding = "UTF-8") // todo <- properties generada con UTF-8 para acentos->
 public class SpringbootWebappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/co/duvan/springboot/webapp/springboot_webapp/controllers/UserRestController.java
+++ src/main/java/co/duvan/springboot/webapp/springboot_webapp/controllers/UserRestController.java
@@ -1,7 +1,6 @@
 package co.duvan.springboot.webapp.springboot_webapp.controllers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
